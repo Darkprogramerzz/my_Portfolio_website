177a227b2da353836b5d6e5f23734575f722cdc6
Darkprogrammer <war20101130@gmail.com>
2023-12-13 17:13:59 +0530

    E

--- script.js
+++ script.js
@@ -15,3 +15,13 @@ window.onscroll = () => {
         };
     });
 };
+let number = document.getElementById("number");
+let counter = 0;
+setInterval(()=> {
+    if(counter == 65){
+        clearInterval
+    }else{
+        counter += 1;
+        number.innerHTML = counter + "%";
+    }
+},30);
